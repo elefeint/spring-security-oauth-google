e13fee92cc407bc7f165e9a53a9f24672706ca5c
Elena Felder <41136058+elefeint@users.noreply.github.com>
2021-05-30 17:11:55 -0400

    adds server authentication

--- src/main/java/com/example/SampleApp.java
+++ src/main/java/com/example/SampleApp.java
@@ -2,10 +2,18 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
+import org.springframework.security.oauth2.server.resource.web.HeaderBearerTokenResolver;
 
 @SpringBootApplication
 public class SampleApp {
     public static void main(String[] args) {
         SpringApplication.run(SampleApp.class, args);
+    }
+
+    @Bean
+    public BearerTokenResolver xAuthTokenBearerTokenResolver() {
+        return new HeaderBearerTokenResolver("X-Auth-Token");
     }
 }
--- src/main/java/com/example/WebController.java
+++ src/main/java/com/example/WebController.java
@@ -1,8 +1,12 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class WebController {
@@ -13,5 +17,11 @@ public class WebController {
     @GetMapping("/getClientId")
     public String getGoogleClientId() {
         return googleClientId;
+    }
+
+    @GetMapping("/getServerInfo")
+    public Map<String, Object> getServerIdentity(Authentication auth) {
+        Jwt authUser = (Jwt) auth.getPrincipal();
+        return authUser.getClaims();
     }
 }
